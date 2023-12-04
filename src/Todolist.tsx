92617193d3ca2501b9c43bdc79afd044498a4edb
shinjuho2 <sinjuho2@gmail.com>
2023-12-04 23:38:38 +0900

    style, file

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import './App.css';
 import uuid from 'react-uuid'
+import { styled } from 'styled-components';
 
 interface List {
   id: string;
@@ -8,22 +8,47 @@ interface List {
   complete: boolean;
 }
 
+const Container = styled.div`
+  text-align: center;
+`;
+
+const ListContainer = styled.div`
+  max-width: 400px;
+  margin: 0 auto;
+  padding: 20px;
+  background-color: #e3e2e2;
+  border-radius: 20px;
+`;
+
+const ItemList = styled.li`
+  list-style-type: none;
+  margin: 10px 0;
+  padding: 10px;
+  border: 1px solid #ccc;
+  background-color: #fff;
+  display: flex;
+  align-items: center;
+  border-radius: 20px;
+`;
+
+const InputList = styled.input`
+  margin-right: 10px;
+`;
+
+const ButtonList = styled.button`
+  margin-left: 10px;
+`;
+
 function Todolist() {
 
-  const [todolist, setTodolist] = useState<List[]>([
-    {
-      id: uuid(),
-      text: "예시 1",
-      complete: false,
-    },
-  ]);
+  const [todolist, setTodolist] = useState<List[]>([]);
 
   const [newList, setNewList] = useState("");
   const [editList, setEditList] = useState<{ id: string; text: string } | null>(null);
 
   useEffect(() => {
     const data = localStorage.getItem('todolist');
-    if(data) {
+    if (data) {
       setTodolist(JSON.parse(data));
     }
   }, [])
@@ -37,25 +62,34 @@ function Todolist() {
   }
 
   const createList = () => {
-    if(newList !== '') {
+    if (newList !== '') {
       setTodolist([...todolist, { id: uuid(), text: newList, complete: false }]);
       setNewList("");
+      localStorage.setItem('todolist', JSON.stringify([...todolist, newList]))
     }
   }
 
+  const completeList = (id: string) => {
+    setTodolist((todolist) =>
+      todolist.map((todo) => (todo.id === id ? { ...todo, complete: true } : todo))
+    )
+
+    localStorage.setItem('todolist', JSON.stringify(todolist.map((todo) => (todo.id === id ? { ...todo, complete: true } : todo))))
+  }
   const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === 'Enter') {
       createList();
     }
   };
 
-  const deleteList = () => {
-    setTodolist((todolist) => todolist.filter((todo) => todo.id !== todo.id));
+  const deleteList = (id: string) => {
+    setTodolist((todolist) => todolist.filter((todo) => todo.id !== id));
+    localStorage.setItem('todolist', JSON.stringify(todolist.filter((todo) => todo.id !== id)))
   }
 
   const edit = (id: string, text: string) => {
-    setEditList({id, text});
-  } 
+    setEditList({ id, text });
+  }
 
   const editCancel = () => {
     setEditList(null);
@@ -63,61 +97,62 @@ function Todolist() {
 
   const editSave = () => {
     if (editList) {
-      const updateList = todolist.map((todo) => 
-        todo.id === editList.id ? {...todo, text: editList.text} : todo
+      const updateList = todolist.map((todo) =>
+        todo.id === editList.id ? { ...todo, text: editList.text } : todo
       );
       setTodolist(updateList);
       setEditList(null);
+      localStorage.setItem('todolist', JSON.stringify(updateList))
     }
   }
 
   return (
-    <div className="App">
-      <div className='Container'>
+    <Container>
+      <ListContainer>
         <h1>my todolist!</h1>
         {
           todolist.map((todo) => {
             return (
               <div>
-                <li className='todolist'>
+                <ItemList key={todo.id}>
                   {
                     editList?.id === todo.id ? (
                       <>
-                        <input 
-                        type="text"
-                        value={editList.text}
-                        onChange={(e) => setEditList({...editList, text: e.target.value})}
+                        <InputList
+                          type="text"
+                          value={editList.text}
+                          onChange={(e) => setEditList({ ...editList, text: e.target.value })}
                         />
-                        <button onClick={editSave}>저장</button>
-                        <button onClick={editCancel}>취소</button>
+                        <ButtonList onClick={editSave}>저장</ButtonList>
+                        <ButtonList onClick={editCancel}>취소</ButtonList>
                       </>
                     )
-                    :
-                    (
-                      <>
-                        <button>완료</button>
-                        {todo.text}
-                        <button onClick={() => edit(todo.id, todo.text)}>수정</button>
-                        <button onClick={() => deleteList()}>삭제</button>
-                      </>
-                    )
+                      :
+                      (
+                        <>
+                          <ButtonList onClick={() => completeList(todo.id)}>완료</ButtonList>
+                          {todo.text}
+                          <ButtonList onClick={() => edit(todo.id, todo.text)}>수정</ButtonList>
+                          <ButtonList onClick={() => deleteList(todo.id)}>삭제</ButtonList>
+                        </>
+                      )
                   }
-                </li>
+                </ItemList>
               </div>
             )
           })
         }
         <div className='createList'>
-          <input 
-          placeholder='입력해주세요'
-          value={newList}
-          onChange={inputChange}
-          onKeyDown={handleKeyPress}
+          <input
+            placeholder='입력해주세요'
+            value={newList}
+            onChange={inputChange}
+            onKeyDown={handleKeyPress}
           />
           <button onClick={createList}>등록</button>
         </div>
-      </div>
-    </div>
+      </ListContainer>
+    </Container>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './Todolist';
-import reportWebVitals from './reportWebVitals';
+import Todolist from './Todolist';
+import GlobalStyles from './GlobalStyles';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <GlobalStyles/>
+    <Todolist/>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
